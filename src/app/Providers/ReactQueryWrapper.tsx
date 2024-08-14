56d09be0d6827e70ed7749a034f7d621e05fe10e
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-14 12:03:42 +0530

    React query wrapper created for potential future use.

---
+++ src/app/Providers/ReactQueryWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface ReactQueryWrapperProps {
+  children: React.ReactNode;
+}
+
+export const ReactQueryWrapper: React.FC<ReactQueryWrapperProps> = ({
+  children,
+}) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { ReactQueryWrapper } from "./Providers/ReactQueryWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <ReactQueryWrapper>
+        <body className={montserrat.className}>{children}</body>
+      </ReactQueryWrapper>
     </html>
   );
 }
