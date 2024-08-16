603155a7afc5c14bbbe4df02b3e6011bd123c2e6
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-16 11:20:37 +0530

    Components imported on to page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,48 @@
 import axios from "axios";
 import { BASE_URL } from "./utils/constants";
-import { ApiDataAttributes, PrismaProductOutput } from "./utils/types";
+import { ApiDataAttributes } from "./utils/types";
+import { Quantum } from "./components/Quantum/Quantum";
+import { Future } from "./components/Future/Future";
+import { Galactic } from "./components/Galactic/Galactic";
+import { FeaturedProduct } from "./components/FeaturedProduct/FeaturedProduct";
+import { Highlights } from "./components/Highlights/HIghlights";
+import { Banner } from "./components/Banner/Banner";
 
 export default async function Home() {
   const res = await axios.get(`${BASE_URL}/api/getAllProducts`);
   const data: ApiDataAttributes = res.data;
   const products = data.products!;
 
+  const quantumCollection = products.filter(
+    (item) => item.theme == "Quantum_Realm"
+  );
+
+  const futureCollection = products.filter(
+    (item) => item.theme == "Human_Future"
+  );
+
+  const galacticCollection = products.filter(
+    (item) => item.theme == "Galactic_Urbanite"
+  );
+
+  const highlightsCollection = products.filter(
+    (item) => item.theme == "Highlights"
+  );
+
+  const martianKeyboard = products.find((item) => item.id == 1130);
+
   if (products.length === 0) {
     return <h1>No products here.</h1>;
   }
 
   return (
-    <ul>
-      {products.map((item, index) => {
-        return <h1 key={index}>{item.name}</h1>;
-      })}
-    </ul>
+    <div className={`w-full flex flex-col gap-[80px]`}>
+      <Banner/>
+      <Quantum items={quantumCollection} />
+      <Future items={futureCollection} />
+      <Galactic items={galacticCollection} />
+      <FeaturedProduct item={martianKeyboard!} />
+      <Highlights items={highlightsCollection} />
+    </div>
   );
 }
