cfbc7a3c413c355989f5663bd6a656b2027f2503
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-16 11:20:07 +0530

    Minor changes.

--- src/app/api/getAllProducts/route.ts
+++ src/app/api/getAllProducts/route.ts
@@ -2,8 +2,6 @@ import { prisma } from "@/app/lib/prisma";
 import { apiResponse } from "@/app/utils/helpers/apiResponse";
 
 export async function GET(req: Request): Promise<Response> {
-    const allProducts = await prisma.product.findMany({
-        take:10
-    })
+    const allProducts = await prisma.product.findMany()
     return apiResponse({ products: allProducts }, 200)
 }
