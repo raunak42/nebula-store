9cbd04ae6ae94d71c1cdbfded9321123d39d5a21
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-17 14:54:09 +0530

    Main loading component.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Loading: React.FC = () => {
-  return <div className="w-full h-[100vh]" >Loading...</div>;
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <Image
+        className="animate-spin"
+        alt="img"
+        width={80}
+        height={80}
+        src={"/galaxy.svg"}
+      />
+    </div>
+  );
 };
 
 export default Loading;
