514cf00e9d35f38036e3be52622fd1b39e1c17fc
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-17 14:48:31 +0530

    Some design changes and logic modifications.

--- src/app/components/ProductCard.tsx/ProductCard.tsx
+++ src/app/components/ProductCard.tsx/ProductCard.tsx
@@ -2,39 +2,62 @@
 import { PrismaProductOutput } from "@/app/utils/types";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { useSetRecoilState } from "recoil";
+import { productDetailsState, showNotificationState } from "@/store";
 
 interface ProductCardProps {
-  item: PrismaProductOutput;
+  product: PrismaProductOutput;
   index: number;
 }
 
-export const ProductCard: React.FC<ProductCardProps> = ({ item, index }) => {
+export const ProductCard: React.FC<ProductCardProps> = ({ product, index }) => {
+  const setProductDetails = useSetRecoilState(productDetailsState);
+  const setShowNotification = useSetRecoilState(showNotificationState);
+  const handleClick = () => {};
+
   return (
     <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.25 }}
-      transition={{ duration: 0.25, delay: 0.1 * index }}
-      className="w-[270px]  h-[400px]  flex flex-col items-center group cursor-pointer "
+      onClick={handleClick}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
     >
-      <div className="flex flex-col w-[270px] h-[320px] items-center justify-center   ">
-        <div className="dark:bg-white overflow-hidden size-[96%] group-hover:size-full group-hover:shadow-2xl rounded-lg ease-in-out  transition-all duration-300 flex items-center justify-center">
-          <Image
-            className="w-[70%] rounded-lg"
-            alt="img"
-            width={100}
-            height={100}
-            src={item.imageLink!}
-          />
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 0.25, delay: 0.1 * index }}
+        className="w-[270px]  h-[440px] gap-[16px]  flex flex-col items-center"
+      >
+        <Link
+          href={`/product/${product.id}`}
+          className="flex flex-col w-[270px] h-[320px] items-center group cursor-pointer justify-center   "
+        >
+          <div className="dark:bg-white overflow-hidden size-[96%] group-hover:size-full group-hover:shadow-2xl rounded-lg ease-in-out  transition-all duration-300 flex items-center justify-center">
+            <Image
+              className="w-[70%] rounded-lg"
+              alt="img"
+              width={100}
+              height={100}
+              src={product.imageLink!}
+            />
+          </div>
+        </Link>
+        <div className="flex h-[100px] w-full gap-[8px] mt-auto flex-col items-center dark:text-white  ">
+          <h1 className="line-clamp-1">{product.name}</h1>
+          <h1>₹ {product.price}.00</h1>
+          <button
+            onClick={() => {
+              setProductDetails(product);
+              setShowNotification(true);
+            }}
+            className="border-[0.5px] border-black hover:border-[1px] hover:shadow-lg rounded-sm w-[100px] h-[30px] flex items-center justify-center text-sm font-light"
+          >
+            Add to cart
+          </button>
         </div>
-      </div>
-      <div className="flex h-[60px] mt-auto flex-col items-center dark:text-white  ">
-        <h1 className="w-full text-start line-clamp-1">{item.name}</h1>
-        <div className="flex items-center justify-start gap-[16px]  w-full mt-auto">
-          <h1>₹{item.price}/-</h1>
-          <Image alt="add" width={24} height={24} src={"/add.svg"} />
-        </div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
