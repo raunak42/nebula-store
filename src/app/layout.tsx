da4131d5604e649766d0d2a8eee3f84f8c4b8c73
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-17 14:54:33 +0530

    Recoil wrapper and notifications added to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { ReactQueryWrapper } from "./providers/ReactQueryWrapper";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
+import { RecoilWrapper } from "./providers/RecoilWrapper";
+import { Notification } from "./components/Notification/Notification";
+import { validateRequest } from "@/auth";
 
-const inter = Inter({ subsets: ["latin"] });
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Nebula Store",
@@ -14,19 +16,23 @@ export const metadata: Metadata = {
   icons: ["/brandmark.svg"],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { session, user } = await validateRequest();
   return (
     <html lang="en">
       <ReactQueryWrapper>
-        <body className={`${montserrat.className}  dark:text-white`}>
-          <Navbar />
-          <div className="dark:bg-[#000000]"> {children}</div>
-          <Footer />
-        </body>
+        <RecoilWrapper>
+          <body className={`${montserrat.className}  dark:text-white`}>
+            <Navbar session={session} user={user} />
+            <Notification session={session} user={user} />
+            <div className="dark:bg-[#000000]"> {children}</div>
+            <Footer />
+          </body>
+        </RecoilWrapper>
       </ReactQueryWrapper>
     </html>
   );
