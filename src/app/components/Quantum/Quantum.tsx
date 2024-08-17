bf20b3dedcef4a45023fb7b2a7eb498a87935612
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-17 14:47:49 +0530

    Auto scrolling behaviour added.

--- src/app/components/Quantum/Quantum.tsx
+++ src/app/components/Quantum/Quantum.tsx
@@ -2,14 +2,50 @@
 import { PrismaProductOutput } from "@/app/utils/types";
 import { ProductCard } from "../ProductCard.tsx/ProductCard";
 import { TextAnimator } from "@/app/animators/TextAnimator/TextAnimator";
+import { useEffect, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
+import { quantumClickedState } from "@/store";
 
 interface QuantumProps {
   items: PrismaProductOutput[];
 }
 
 export const Quantum: React.FC<QuantumProps> = ({ items }) => {
+  const quantumRef = useRef<HTMLDivElement | null>(null);
+  const [quantumClicked, setQuantumClicked] =
+    useRecoilState(quantumClickedState);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  
+  useEffect(() => {
+    const quantumElement = quantumRef.current;
+    const scrollTo = localStorage.getItem("scroll-to");
+
+    if (scrollTo === "quantum" || quantumClicked) {
+      quantumElement?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }, [quantumClicked]);
+
+  useEffect(() => {
+    const quantumElement = quantumRef.current;
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+    if (quantumElement) observer.observe(quantumElement);
+
+    if (isIntersecting) {
+      localStorage.clear();
+    }
+    return () => {
+      if (quantumElement) observer.unobserve(quantumElement);
+    };
+  }, [isIntersecting]);
+
   return (
-    <div id="quantum">
+    <div ref={quantumRef} id="quantum" className="mt-[-60px]">
       <div className="w-full flex flex-col items-center justify-center gap-[8px]">
         <TextAnimator
           classname="text-5xl "
@@ -24,9 +60,7 @@ export const Quantum: React.FC<QuantumProps> = ({ items }) => {
       </div>
       <div className=" w-full flex flex-row flex-wrap items-center justify-between px-[24px] mt-[16px] ">
         {items.map((item, index) => {
-          return (
-              <ProductCard index={index} item={item} key={index} />
-          );
+          return <ProductCard index={index} product={item} key={index} />;
         })}
       </div>
     </div>
