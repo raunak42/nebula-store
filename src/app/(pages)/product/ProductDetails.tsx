3e40efcbb880dcd4436dc7654401f465ae720567
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 10:50:18 +0530

    Addind, removing, deleting functionality added.

--- src/app/(pages)/product/ProductDetails.tsx
+++ src/app/(pages)/product/ProductDetails.tsx
@@ -1,17 +1,49 @@
 "use client";
-import { PrismaProductOutput } from "@/app/utils/types";
-import { showNotificationState } from "@/store";
+import { AddToCartParams } from "@/app/api/addToCart/route";
+import { BASE_URL } from "@/app/utils/constants";
+import { ApiDataAttributes, PrismaProductOutput } from "@/app/utils/types";
+import {
+  productDetailsState,
+  productQtyState,
+  showNotificationState,
+} from "@/store";
+import { Session } from "lucia";
 import Image from "next/image";
 import { useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 interface ProductDetailsProps {
   product: PrismaProductOutput;
+  session: Session | null;
 }
 
-export const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
-  const [counter, setCounter] = useState<number>(1);
+export const ProductDetails: React.FC<ProductDetailsProps> = ({
+  product,
+  session,
+}) => {
+  const setProductDetails = useSetRecoilState(productDetailsState);
+  const [quantity, setQuantity] = useRecoilState<number>(productQtyState);
   const setShowNotification = useSetRecoilState(showNotificationState);
+  const [showSpinner, setShowSpinner] = useState<boolean>(false);
+
+  const addToCart = async () => {
+    const body: AddToCartParams = {
+      productId: product.id!,
+      userId: session?.userId!,
+      quantity: quantity,
+    };
+
+    const res = await fetch(`${BASE_URL}/api/addToCart`, {
+      method: "POST",
+      cache: "no-store",
+      body: JSON.stringify(body),
+    });
+    const data: ApiDataAttributes = await res.json();
+    if (data.message === "Success") {
+      setShowNotification(true);
+      setShowSpinner(false);
+    }
+  };
 
   return (
     <div className="w-[70%] flex flex-col gap-[12px]">
@@ -28,24 +60,43 @@ export const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
       <div className="flex flex-col gap-[24px] mt-[32px]">
         <div className="h-[40px] w-[120px] bg-white text-black rounded-none border flex items-center justify-center gap-[18px]">
           <button
-            className={`${counter === 1 && "cursor-not-allowed"}`}
+            className={`${quantity === 1 && "cursor-not-allowed"}`}
             onClick={() => {
-              if (counter !== 1) setCounter((prev) => prev - 1);
+              if (quantity !== 1) setQuantity((prev) => prev - 1);
             }}
           >
             <Image alt="" width={18} height={18} src={"/minus.svg"} />
           </button>
-          <h1 className="w-[20%] flex items-center justify-center" >{counter}</h1>
+          <h1 className="w-[20%] flex items-center justify-center">
+            {quantity}
+          </h1>
           <button
             onClick={() => {
-              setCounter((prev) => prev + 1);
+              setQuantity((prev) => prev + 1);
             }}
           >
             <Image alt="" width={18} height={18} src={"/plus.svg"} />
           </button>
         </div>
-        <button onClick={()=>setShowNotification(true)} className="h-[40px] w-[300px] bg-black text-white rounded-sm">
-          Add to cart
+        <button
+          onClick={() => {
+            setShowSpinner(true);
+            addToCart();
+            setProductDetails(product);
+          }}
+          className="h-[40px] w-[300px] bg-black flex items-center justify-center rounded-sm"
+        >
+          {showSpinner ? (
+            <Image
+              alt=""
+              width={24}
+              height={24}
+              src={"/spinner-white.svg"}
+              className="animate-spin"
+            />
+          ) : (
+            <h1 className="text-white"> Add to cart</h1>
+          )}
         </button>
         <p>{product?.description}</p>
       </div>
