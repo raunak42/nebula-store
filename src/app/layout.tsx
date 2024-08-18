0e8f3dbb321cfd82bdde6fc38d5ca801bc986bff
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 10:51:35 +0530

    Makes an api call to get user details.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import { Footer } from "./components/Footer/Footer";
 import { RecoilWrapper } from "./providers/RecoilWrapper";
 import { Notification } from "./components/Notification/Notification";
 import { validateRequest } from "@/auth";
+import { BASE_URL } from "./utils/constants";
+import { GetUserApiBodyProps } from "./api/getUserDetails/route";
+import { ApiDataAttributes } from "./utils/types";
 
 const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
 
@@ -22,12 +25,26 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const { session, user } = await validateRequest();
+  let userDetails;
+  if (session) {
+    const body: GetUserApiBodyProps = {
+      userId: session?.userId!,
+    };
+    const res = await fetch(`${BASE_URL}/api/getUserDetails`, {
+      method: "POST",
+      cache: "no-store",
+      body: JSON.stringify(body),
+    });
+    const data: ApiDataAttributes = await res.json();
+    userDetails = data.user;
+  }
+
   return (
     <html lang="en">
       <ReactQueryWrapper>
         <RecoilWrapper>
           <body className={`${montserrat.className}  dark:text-white`}>
-            <Navbar session={session} user={user} />
+            <Navbar userDetails={userDetails!} session={session} />
             <Notification session={session} user={user} />
             <div className="dark:bg-[#000000]"> {children}</div>
             <Footer />
