863c6a87143138c2ea99f3f465888cce00e07081
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 20:25:17 +0530

    Minor change.

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,4 +1,4 @@
 const BASE_URL_DEV = "http://localhost:3000"
-const BASE_URL_PROD = "https://nebulastore.raunak42.in"
+const BASE_URL_PROD = "https://nebula.raunak42.in"
 
 export const BASE_URL = (process.env.NODE_ENV === "production" ? BASE_URL_PROD : BASE_URL_DEV)
