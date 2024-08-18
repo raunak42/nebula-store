215e7877b53f6032322523983bb04d007b8915db
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 20:11:25 +0530

    Minor refactoring.

--- src/app/(pages)/signout/page.tsx
+++ src/app/(pages)/signout/page.tsx
@@ -1,10 +1,10 @@
 import { logout } from "@/app/lib/actions";
+import { SignOutButton } from "./signOutButton";
 
 export default async function Page() {
   return (
-    <form action={logout}>
-      <button className="p-4 rounded-full border-2 border-black">Sign out</button>
+    <form className="w-full h-[50vh] mt-[80px] p-[32px]" action={logout}>
+      <SignOutButton />{" "}
     </form>
   );
 }
-
