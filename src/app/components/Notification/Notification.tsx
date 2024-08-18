d0b97bd627bf82e8a7e4a15dc9036a1b18969493
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 10:46:53 +0530

    Notification now lets you log in with google.

--- src/app/components/Notification/Notification.tsx
+++ src/app/components/Notification/Notification.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { productDetailsState, showNotificationState } from "@/store";
+import {
+  productDetailsState,
+  productQtyState,
+  showNotificationState,
+} from "@/store";
 import { Session, User } from "lucia";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,14 +18,19 @@ export const Notification: React.FC<NotificationProps> = ({
   session,
   user,
 }) => {
-  const product = useRecoilValue(productDetailsState);
+  const [product, setProduct] = useRecoilState(productDetailsState);
   const [showNotification, setShowNotification] = useRecoilState(
     showNotificationState
   );
+  const quantity = useRecoilValue(productQtyState);
   return (
     <div className={`w-full flex items-start justify-end `}>
       <div
-        className={`z-30 w-[340px]  border bg-white rounded-sm shadow-2xl mt-[-300px] fixed top-[12px] right-[96px] flex flex-col items-center justify-start ${session?"gap-[32px] p-[24px] h-[360px]":"gap-[16px] py-[4px] px-[24px] h-[180px]"} transition-all duration-300 ease-in-out ${
+        className={`z-10 w-[340px]  border bg-white rounded-sm shadow-2xl mt-[-300px] fixed top-[12px] right-[96px] flex flex-col items-center justify-start ${
+          session
+            ? "gap-[32px] p-[24px] h-[360px]"
+            : "gap-[16px] py-[4px] px-[24px] h-[180px]"
+        } transition-all duration-300 ease-in-out ${
           showNotification ? "opacity-100 translate-y-[380px] " : " opacity-0"
         }  `}
       >
@@ -36,7 +45,12 @@ export const Notification: React.FC<NotificationProps> = ({
               <h1 className="text-xs">Item added to cart</h1>
             </div>
           )}
-          <button onClick={() => setShowNotification(false)}>
+          <button
+            onClick={() => {
+              setShowNotification(false);
+              setProduct(null);
+            }}
+          >
             <Image alt="" width={24} height={24} src={"/cross.svg"} />
           </button>
         </div>
@@ -45,14 +59,20 @@ export const Notification: React.FC<NotificationProps> = ({
           <>
             <div className=" h-[200px] w-full flex items-start justify-start gap-[24px]">
               <Image alt="" width={60} height={60} src={product?.imageLink!} />
-              <div className="flex flex-col gap-[4px] w-[60%]">
+              <div className="flex flex-col gap-[4px] w-[50%]">
                 <h1 className="text-sm">{product?.name}</h1>
                 <h1 className="text-sm">₹ {product?.price}.00</h1>
               </div>
+              <h1 className="text-md font-semibold ">x {quantity}</h1>
             </div>
 
             <div className="  flex flex-col items-center justify-start gap-[12px]">
-              <button className="w-[300px] h-[50px] bg-white border-[1.5px] border-black rounded-full">
+              <button
+                onClick={() => {
+                  window.location.assign("/cart");
+                }}
+                className="w-[300px] h-[50px] bg-white border-[1.5px] border-black rounded-full"
+              >
                 View cart
               </button>
               <button className="w-[300px] h-[50px] bg-black rounded-full text-white">
@@ -75,7 +95,13 @@ export const Notification: React.FC<NotificationProps> = ({
               href={"/login/google"}
               className="shadow-xl rounded-full flex items-center justify-center w-full  border-[1.5px] hover:border-[2px] border-black"
             >
-              <Image className="" alt="google" height={42} width={42} src={"/google.svg"} />
+              <Image
+                className=""
+                alt="google"
+                height={42}
+                width={42}
+                src={"/google.svg"}
+              />
               <h1>Continue with google</h1>
             </Link>
           </div>
