74f33318b31884448c7fc3fee38d1bbf835f74fc
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 10:44:52 +0530

    Updated.

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -1,8 +1,11 @@
 import { Prisma } from "@prisma/client";
 
 export type PrismaProductOutput = Partial<Prisma.ProductGetPayload<{ select: {}; include: {} }>>
+export type PrismaUserOutput = Partial<Prisma.UserGetPayload<{ select: {}, include: {} }>>
 
 export interface ApiDataAttributes {
     products?: PrismaProductOutput[]
     product?: PrismaProductOutput
+    message?: string
+    user?: PrismaUserOutput
 }
