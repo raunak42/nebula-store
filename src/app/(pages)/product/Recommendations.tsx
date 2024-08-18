a4140eed8d0044e6c7c296cd2754916e85096ee7
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-18 10:49:39 +0530

    Minor changes.

--- src/app/(pages)/product/Recommendations.tsx
+++ src/app/(pages)/product/Recommendations.tsx
@@ -1,12 +1,14 @@
 import { ProductCard } from "@/app/components/ProductCard.tsx/ProductCard";
 import { PrismaProductOutput } from "@/app/utils/types";
+import { validateRequest } from "@/auth";
 
 interface RecommendationsProps {
   products: PrismaProductOutput[];
 }
-export const Recommendations: React.FC<RecommendationsProps> = ({
+export const Recommendations: React.FC<RecommendationsProps> = async({
   products,
 }) => {
+  const {session, user} = await validateRequest()
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <h1 className="text-4xl text-center w-full p-[24px]">
@@ -14,7 +16,7 @@ export const Recommendations: React.FC<RecommendationsProps> = ({
       </h1>
       <div className=" w-full flex flex-row flex-wrap items-center justify-center gap-[60px] px-[24px] mt-[16px] ">
         {products.map((item, index) => {
-          return <ProductCard index={index} product={item} key={index} />;
+          return <ProductCard session={session} index={index} product={item} key={index} />;
         })}
       </div>
     </div>
