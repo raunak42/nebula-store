fe6c69eb4e701aef48c5583aa0511a8edab4a87c
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-19 14:47:09 +0530

    More safety added.

--- src/app/api/getAllProducts/route.ts
+++ src/app/api/getAllProducts/route.ts
@@ -2,6 +2,12 @@ import { prisma } from "@/app/lib/prisma";
 import { apiResponse } from "@/app/utils/helpers/apiResponse";
 
 export async function GET(req: Request): Promise<Response> {
-    const allProducts = await prisma.product.findMany()
-    return apiResponse({ products: allProducts }, 200)
+    try {
+        const allProducts = await prisma.product.findMany()
+        return apiResponse({ products: allProducts, message: "Success" }, 200)
+    } catch (error) {
+        console.log(error)
+        return apiResponse({ message: "Error" })
+    }
+
 }
