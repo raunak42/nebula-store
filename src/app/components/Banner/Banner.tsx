bd3d32cb1a377465161a22ded57fdc4151239082
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-31 02:34:13 +0530

    Modified with promise so that image loads first then text appears.

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -3,8 +3,18 @@ import { TextAnimator } from "@/app/animators/TextAnimator/TextAnimator";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import banner47 from "@/../public/banner47.png";
+import { useState, useEffect } from "react";
 
 export const Banner: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    return new Promise<void>((resolve) => {
+        setImageLoaded(true);
+        resolve();
+    });
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -12,18 +22,31 @@ export const Banner: React.FC = () => {
       transition={{ duration: 0.5 }}
       className="relative w-full flex items-center justify-center"
     >
-      <div className="absolute inset-0 w-full flex items-center justify-center">
-        <div className="w-[60%]">
-          <TextAnimator
-            classname="text-3xl sm:text-6xl lg:text-8xl text-[#ffffff] text-shadow font-semibold text-center flex items-center justify-center "
-            text="Own a piece of the future."
-            space={20}
-          />
-        </div>
-      </div>
       <div className="w-full">
-        <Image alt="img" src={banner47} placeholder="blur" blurDataURL="https://github.com/user-attachments/assets/2f451400-2b3e-4ef1-a197-ba61e1fdf6df" />
+        <Image
+          alt="img"
+          src={banner47}
+          placeholder="blur"
+          blurDataURL="https://github.com/user-attachments/assets/2f451400-2b3e-4ef1-a197-ba61e1fdf6df"
+          onLoadingComplete={handleImageLoad}
+        />
       </div>
+      {imageLoaded && (
+        <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="absolute inset-0 w-full flex items-center justify-center"
+        >
+          <div className="w-[60%]">
+            <TextAnimator
+              classname="text-3xl sm:text-6xl lg:text-8xl text-[#ffffff] text-shadow font-semibold text-center flex items-center justify-center"
+              text="Own a piece of the future."
+              space={20}
+            />
+          </div>
+        </motion.div>
+      )}
     </motion.div>
   );
 };
