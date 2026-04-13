8db672d42d34bb1671d9aadcd5daaa8737ee3818
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2026-04-13 20:42:19 +0530

    Prevent prerendering product API routes

--- src/app/api/getAllProducts/route.ts
+++ src/app/api/getAllProducts/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/app/lib/prisma";
 import { apiResponse } from "@/app/utils/helpers/apiResponse";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request): Promise<Response> {
     try {
         const allProducts = await prisma.product.findMany()
--- src/app/api/getRandomProducts/route.ts
+++ src/app/api/getRandomProducts/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/app/lib/prisma";
 import { apiResponse } from "@/app/utils/helpers/apiResponse";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request): Promise<Response> {
     const randomNumber = Math.floor(Math.random() * 26) + 0; //random number between 0 and 26
     const randomProducts = await prisma.product.findMany({
